567f2864120ac24d3c3faad304cfd83acc335775
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-10-20 01:29:33 -0700

    uppp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,21 @@ import {Button, ButtonGroup} from "@nextui-org/button";
 
 const hero = () => (
  <div className="">
-  <div className="">
+  <div className="flex-row bg-green-600">
+   <div className="">
+   <Image
+      src="/profile.png"
+      width={250}
+      height={250}
+      alt="Picture of Mascot"
+    />
+   </div>
+    <div className="bg-gradient-to-r flex-col"> 
+        <p className="font-bold"> Welcome to <p className="underline-offset-1">Grood</p> </p>
+        <p>The Typing Test Simulator for Ideal Improvement</p>
+    </div>
 
   </div>
-
  </div>
 );
 
