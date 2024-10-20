5abaf02b2e8db448a34c24a16fdcb9813cedf6a7
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-10-19 23:16:39 -0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link"
 import {
   Navbar, 
   NavbarBrand, 
@@ -8,17 +9,41 @@ import {
   NavbarMenu,
   NavbarMenuItem
 } from "@nextui-org/navbar";
+import {Button, ButtonGroup} from "@nextui-org/button";
+
 
 export default function Home() {
   return (
-    <div className=" items-center justify-items-center min-h-screen p-2 pb-4 gap-16">
+    <div className="items-center justify-items-center min-h-screen p-2 pb-4 gap-16">
       <Navbar> 
         <NavbarBrand>
           <p className="font-bold"> Grood </p>
         </NavbarBrand>
          <NavbarContent>
+         <NavbarItem>
+          <Link color="foreground" href="#">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Team
+          </Link>
+        </NavbarItem>
+        </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
          </NavbarContent>
       </Navbar>
+
+      
     </div>
   );
 }
