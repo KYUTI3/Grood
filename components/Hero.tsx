4d8fd1895eebb3ee34f4f830577586c7a1794634
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-10-20 05:09:35 -0700

    i am not slep

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import {Button, ButtonGroup} from "@nextui-org/button";
+import { Button } from "@nextui-org/button";
 
 const Hero = () => (
- <div className="">
-  <div className="flex-row bg-green-600">
-   <div className="">
-   <Image
-      src="/logo.png"
-      width={250}
-      height={250}
-      alt="Picture of Mascot"
-    />
-   </div>
-    <div className="flex-col"> 
-      <div className="font-bold"> 
-        <p className="bg-gradient-to-r from-green-500 to-sky-500 underline-offset-1"> Welcome to Grood </p>
-        <p className="bg-gradient-to-r from-green-500 to-sky-500">The Typing Test Simulator for Ideal Improvement</p>
+  <div className="flex flex-col items-center justify-center min-h-screen bg-green-600 text-white">
+    <div className="flex flex-col items-center">
+      <Image
+        src="/logo.png"
+        width={250}
+        height={250}
+        alt="Picture of Mascot"
+      />
+      <div className="text-center mt-4">
+        <p className="text-4xl font-bold bg-gradient-to-r from-green-500 to-sky-500 bg-clip-text text-transparent underline-offset-1">
+          Welcome to Grood
+        </p>
+        <p className="text-xl bg-gradient-to-r from-green-500 to-sky-500 bg-clip-text text-transparent">
+          The Typing Test Simulator for Ideal Improvement
+        </p>
       </div>
-    <div className="text-black">
-    <Button radius="md" color="success">
-      Sign Up!
-    </Button>
-    </div>
+      <div className="mt-6">
+        <Button radius="md" color="success">
+          Sign Up!
+        </Button>
+      </div>
     </div>
   </div>
- </div>
 );
 
 export default Hero;
