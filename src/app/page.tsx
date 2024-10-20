bbea3353fb58a3b79b31f3945071b257f2b50314
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-10-19 22:30:58 -0700

    slop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import {
+  Navbar, 
+  NavbarBrand, 
+  NavbarContent, 
+  NavbarItem, 
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem
+} from "@nextui-org/navbar";
 
 export default function Home() {
   return (
     <div className=" items-center justify-items-center min-h-screen p-2 pb-4 gap-16">
-      
+      <Navbar> 
+        <NavbarBrand>
+          <p className="font-bold"> Grood </p>
+        </NavbarBrand>
+         <NavbarContent>
+         </NavbarContent>
+      </Navbar>
     </div>
   );
 }
